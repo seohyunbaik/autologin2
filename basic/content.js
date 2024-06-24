6244db4f8fa5ad8f415f0872fa405a8d41f435bc
seohyunbaik <129375413+seohyunbaik@users.noreply.github.com>
2024-06-24 19:05:21 +0900

    Create content.js

---
+++ basic/content.js
@@ -0,0 +1,16 @@
+chrome.action.onClicked.addListener(() => {
+  const userIdInput = document.querySelector('#IdInput');
+  if (userIdInput) userIdInput.value = 'seohyun8646';
+  
+  const passwordSubmitButton = document.querySelector('input[type="submit"][value="비밀번호 인증"]');
+  if (passwordSubmitButton) passwordSubmitButton.click();
+
+  setTimeout(() => {
+    const passwordInput = document.querySelector('#passwordInput');
+    if (passwordInput) {
+      passwordInput.value = '!@SHbike21Kai';
+      const loginButton = document.querySelector('input[type="submit"].loginbtn');
+      if (loginButton) loginButton.click();
+    }
+  }, 1000); // Adjust timing as needed
+});
