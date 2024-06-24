1e5e47250e642dccd0ab6251f7849ba9e96c1d1f
seohyunbaik <129375413+seohyunbaik@users.noreply.github.com>
2024-06-24 19:09:20 +0900

    Update content.js

--- basic/content.js
+++ basic/content.js
@@ -1,16 +1,14 @@
-chrome.action.onClicked.addListener(() => {
-  const userIdInput = document.querySelector('#IdInput');
-  if (userIdInput) userIdInput.value = 'seohyun8646';
-  
-  const passwordSubmitButton = document.querySelector('input[type="submit"][value="비밀번호 인증"]');
-  if (passwordSubmitButton) passwordSubmitButton.click();
+const userIdInput = document.querySelector('#IdInput');
+if (userIdInput) userIdInput.value = 'seohyun8646';
 
-  setTimeout(() => {
-    const passwordInput = document.querySelector('#passwordInput');
-    if (passwordInput) {
-      passwordInput.value = '!@SHbike21Kai';
-      const loginButton = document.querySelector('input[type="submit"].loginbtn');
-      if (loginButton) loginButton.click();
-    }
-  }, 1000); // Adjust timing as needed
-});
+const passwordSubmitButton = document.querySelector('input[type="submit"][value="비밀번호 인증"]');
+if (passwordSubmitButton) passwordSubmitButton.click();
+
+setTimeout(() => {
+  const passwordInput = document.querySelector('#passwordInput');
+  if (passwordInput) {
+    passwordInput.value = '1111';
+    const loginButton = document.querySelector('input[type="submit"].loginbtn');
+    if (loginButton) loginButton.click();
+  }
+}, 1000); // Adjust timing as needed
