efbb118d1300027c68dfa9ec23ab0b3808990b3c
seohyunbaik <129375413+seohyunbaik@users.noreply.github.com>
2024-06-24 19:28:13 +0900

    Create content.js

---
+++ basic/content.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    const inputField = document.getElementById('IdInput');
+    if (inputField) {
+        inputField.value = 'seohyun8646';
+    }
+});
